faab8f301030603dd28985c82b978583e7295c09
Yashas J <yashas.j@dbizsolution.com>
2024-07-30 13:06:40 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logoImg from "../assets/quiz-logo.png"
+
+const Header = () => {
+  return ( 
+  <header>
+    <img src={logoImg} alt='quiz-logo'/>
+    <h1>ReactQuiz</h1>
+  </header>
+  )
+}
+
+export default Header
